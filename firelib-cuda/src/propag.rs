1ba97c6e4b3fe14b8a998572ee8a4714a18c5d30
Alberto Valverde <alberto@toscat.net>
2025-05-28 09:27:55 +0200

    wip

--- firelib-cuda/src/propag.rs
+++ firelib-cuda/src/propag.rs
@@ -62,117 +62,120 @@ pub unsafe fn propag(
     let idx = (idx_2d.x + idx_2d.y * width) as usize;
 
     // Our index into the shared area
-    let shared_ix = thread::thread_idx_x() as usize + thread::thread_idx_y() as usize + BLOCK_WIDTH;
+    let shared_ix = thread::thread_idx_x() as usize + thread::thread_idx_y() as usize * BLOCK_WIDTH;
 
-    ////////////////////////////////////////////////////////////////////////
-    // First phase, load data from global to shared memory
-    ////////////////////////////////////////////////////////////////////////
+    // FIXME: Loop until no thread has worked
+    for _ in (0..1000) {
+        ////////////////////////////////////////////////////////////////////////
+        // First phase, load data from global to shared memory
+        ////////////////////////////////////////////////////////////////////////
 
-    // Read our ref's position from global memory and save it into shared
-    let sref = Coord {
-        x: *refs_x.wrapping_add(idx),
-        y: *refs_y.wrapping_add(idx),
-    };
-    *shared_ref.wrapping_add(shared_ix) = sref;
+        // Read our ref's position from global memory and save it into shared
+        let sref = Coord {
+            x: *refs_x.wrapping_add(idx),
+            y: *refs_y.wrapping_add(idx),
+        };
+        *shared_ref.wrapping_add(shared_ix) = sref;
 
-    // Read our ref's fire from global memory and save it into shared
-    let idx_ref = (sref.x + sref.y * width) as usize;
-    *shared_ref_fire.wrapping_add(shared_ix) = (FireSimpleCuda {
-        azimuth_max: *azimuth_max.wrapping_add(idx_ref),
-        speed_max: *speed_max.wrapping_add(idx_ref),
-        eccentricity: *eccentricity.wrapping_add(idx_ref),
-    })
-    .into();
+        // Read our ref's fire from global memory and save it into shared
+        let idx_ref = (sref.x + sref.y * width) as usize;
+        *shared_ref_fire.wrapping_add(shared_ix) = (FireSimpleCuda {
+            azimuth_max: *azimuth_max.wrapping_add(idx_ref),
+            speed_max: *speed_max.wrapping_add(idx_ref),
+            eccentricity: *eccentricity.wrapping_add(idx_ref),
+        })
+        .into();
 
-    // Read our time from global memory and save it into shared
-    *shared_time.wrapping_add(shared_ix) = *time.wrapping_add(idx);
-    // Read our fire from global memory and save it into shared
-    *shared_fire.wrapping_add(shared_ix) = (FireSimpleCuda {
-        azimuth_max: *azimuth_max.wrapping_add(idx),
-        speed_max: *speed_max.wrapping_add(idx),
-        eccentricity: *eccentricity.wrapping_add(idx),
-    })
-    .into();
+        // Read our time from global memory and save it into shared
+        *shared_time.wrapping_add(shared_ix) = *time.wrapping_add(idx);
+        // Read our fire from global memory and save it into shared
+        *shared_fire.wrapping_add(shared_ix) = (FireSimpleCuda {
+            azimuth_max: *azimuth_max.wrapping_add(idx),
+            speed_max: *speed_max.wrapping_add(idx),
+            eccentricity: *eccentricity.wrapping_add(idx),
+        })
+        .into();
 
-    // Wait until all other threads have reached this point so we can safely
-    // read from shared memory
-    thread::sync_threads();
+        // Wait until all other threads have reached this point so we can safely
+        // read from shared memory
+        thread::sync_threads();
 
-    ////////////////////////////////////////////////////////////////////////
-    // Begin neighbor analysys
-    ////////////////////////////////////////////////////////////////////////
-    for d_i in -1..2 {
-        for d_j in -1..2 {
-            if d_i == 0 && d_j == 0 {
-                // Don't analyze ourselves
-                continue;
-            }
-            let n_i = thread::thread_idx_x() as i32 + d_i;
-            let n_j = thread::thread_idx_y() as i32 + d_j;
-            // Neighbor global pos
-            let n_pos = Coord {
-                x: pos.x as i32 + d_i,
-                y: pos.y as i32 + d_j,
-            };
-            // If this neighbor is outside this block or global area
-            // continue
-            if n_i < 0
-                || n_i >= BLOCK_WIDTH as _
-                || n_j < 0
-                || n_j >= BLOCK_HEIGHT as _
-                || n_pos.x < 0
-                || n_pos.x >= width as _
-                || n_pos.y < 0
-                || n_pos.y >= height as _
-            {
-                continue;
-            }
+        ////////////////////////////////////////////////////////////////////////
+        // Begin neighbor analysys
+        ////////////////////////////////////////////////////////////////////////
+        for d_i in -1..2 {
+            for d_j in -1..2 {
+                if d_i == 0 && d_j == 0 {
+                    // Don't analyze ourselves
+                    continue;
+                }
+                let n_i = thread::thread_idx_x() as i32 + d_i;
+                let n_j = thread::thread_idx_y() as i32 + d_j;
+                // Neighbor global pos
+                let n_pos = Coord {
+                    x: pos.x as i32 + d_i,
+                    y: pos.y as i32 + d_j,
+                };
+                // If this neighbor is outside this block or global area
+                // continue
+                if n_i < 0
+                    || n_i >= BLOCK_WIDTH as _
+                    || n_j < 0
+                    || n_j >= BLOCK_HEIGHT as _
+                    || n_pos.x < 0
+                    || n_pos.x >= width as _
+                    || n_pos.y < 0
+                    || n_pos.y >= height as _
+                {
+                    continue;
+                }
 
-            let n_i = n_i as usize;
-            let n_j = n_j as usize;
-            let n_pos = Coord {
-                x: n_pos.x as usize,
-                y: n_pos.y as usize,
-            };
-            // Index of our neighbor in shared memory
-            let shared_neigh_ix = n_i + n_j * BLOCK_WIDTH;
-            // Read neighbor time from shared mem
-            let neigh_time: float::T = *shared_time.wrapping_add(shared_neigh_ix);
+                let n_i = n_i as usize;
+                let n_j = n_j as usize;
+                let n_pos = Coord {
+                    x: n_pos.x as usize,
+                    y: n_pos.y as usize,
+                };
+                // Index of our neighbor in shared memory
+                let shared_neigh_ix = n_i + n_j * BLOCK_WIDTH;
+                // Read neighbor time from shared mem
+                let neigh_time: float::T = *shared_time.wrapping_add(shared_neigh_ix);
 
-            if neigh_time < max_time {
-                // neighbor has fire
-                let neigh_ref_pos = *shared_ref.wrapping_add(shared_neigh_ix);
-                let neigh_ref_fire = &(*shared_ref_fire.wrapping_add(shared_neigh_ix));
-                let bearing = Angle::new::<radian>(geo_ref.bearing(
-                    Coord {
-                        x: neigh_ref_pos.x as _,
-                        y: neigh_ref_pos.y as _,
-                    },
-                    Coord {
-                        x: pos.x as _,
-                        y: pos.y as _,
-                    },
-                ) as _);
-                //TODO
+                if neigh_time < max_time {
+                    // neighbor has fire
+                    let neigh_ref_pos = *shared_ref.wrapping_add(shared_neigh_ix);
+                    let neigh_ref_fire = &(*shared_ref_fire.wrapping_add(shared_neigh_ix));
+                    let bearing = Angle::new::<radian>(geo_ref.bearing(
+                        Coord {
+                            x: neigh_ref_pos.x as _,
+                            y: neigh_ref_pos.y as _,
+                        },
+                        Coord {
+                            x: pos.x as _,
+                            y: pos.y as _,
+                        },
+                    ) as _);
+                    //TODO
+                }
             }
         }
-    }
 
-    let terrain = TerrainCuda {
-        d1hr: d1hr[idx],
-        d10hr: d10hr[idx],
-        d100hr: d100hr[idx],
-        herb: herb[idx],
-        wood: wood[idx],
-        wind_speed: wind_speed[idx],
-        wind_azimuth: wind_azimuth[idx],
-        slope: slope[idx],
-        aspect: aspect[idx],
-    };
-    if let Some(fire) = Catalog::STANDARD.burn_simple(model[idx], &terrain.into()) {
-        let fire = Into::<FireSimpleCuda>::into(fire);
-        *time.wrapping_add(idx) = fire.speed_max;
+        let terrain = TerrainCuda {
+            d1hr: d1hr[idx],
+            d10hr: d10hr[idx],
+            d100hr: d100hr[idx],
+            herb: herb[idx],
+            wood: wood[idx],
+            wind_speed: wind_speed[idx],
+            wind_azimuth: wind_azimuth[idx],
+            slope: slope[idx],
+            aspect: aspect[idx],
+        };
+        if let Some(fire) = Catalog::STANDARD.burn_simple(model[idx], &terrain.into()) {
+            let fire = Into::<FireSimpleCuda>::into(fire);
+            *time.wrapping_add(idx) = fire.speed_max;
+        }
+        *refs_x.wrapping_add(idx) = pos.x;
+        *refs_y.wrapping_add(idx) = pos.y;
     }
-    *refs_x.wrapping_add(idx) = pos.x;
-    *refs_y.wrapping_add(idx) = pos.y;
 }

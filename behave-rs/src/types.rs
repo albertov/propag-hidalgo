990759b7152cc1b414a040c5377fff6089e19684
Alberto Valverde <alberto@toscat.net>
2025-05-28 09:27:55 +0200

    heapless

--- behave-rs/src/types.rs
+++ behave-rs/src/types.rs
@@ -1,3 +1,5 @@
+use heapless::String;
+use heapless::Vec;
 use uom::si::f64::*;
 use uom::si::length::foot;
 use uom::si::ratio::ratio;
@@ -96,104 +98,104 @@ pub enum SizeClass {
 }
 
 pub struct FuelDef {
-    pub name: String,
-    pub desc: String,
+    pub name: String<16>,
+    pub desc: String<64>,
     pub depth: Length,
     pub mext: Ratio,
     pub adjust: Ratio,
-    pub particles: Vec<ParticleDef>,
+    pub particles: Vec<ParticleDef, 20>,
 }
 
 #[derive(Clone)]
 pub struct Fuel {
-    pub name: String,
-    pub desc: String,
+    pub name: String<16>,
+    pub desc: String<64>,
     pub depth: f64,
     pub mext: f64,
     pub adjust: f64,
-    pub alive_particles: Vec<Particle>,
-    pub dead_particles: Vec<Particle>,
+    pub alive_particles: Vec<Particle, 20>,
+    pub dead_particles: Vec<Particle, 20>,
 }
 
-pub type Catalog = Vec<Fuel>;
+pub type Catalog = Vec<Fuel, 20>;
 
 lazy_static::lazy_static! {
-    pub static ref STANDARD_CATALOG : Catalog = {
-    vec![
+pub static ref STANDARD_CATALOG : Catalog = {
+    Vec::from_slice(&[
         Fuel::make(FuelDef {
-            name: String::from("NoFuel"),
-            desc: String::from("No Combustible Fuel"),
+            name: String::try_from("NoFuel").unwrap(),
+            desc: String::try_from("No Combustible Fuel").unwrap(),
             depth: Length::new::<foot>(0.1),
             mext: Ratio::new::<ratio>(0.01),
             adjust: Ratio::new::<ratio>(1.0),
-            particles: vec![],
+            particles: Vec::new(),
         }),
         Fuel::make(FuelDef {
-            name: String::from("NFFL01"),
-            desc: String::from("Short Grass (1 ft)"),
+            name: String::try_from("NFFL01").unwrap(),
+            desc: String::try_from("Short Grass (1 ft)").unwrap(),
             depth: Length::new::<foot>(1.0),
             mext: Ratio::new::<ratio>(0.12),
             adjust: Ratio::new::<ratio>(1.0),
-            particles: vec![ParticleDef::standard(ParticleType::Dead, 0.0340, 3500.0)],
+            particles: Vec::from_slice(&[ParticleDef::standard(ParticleType::Dead, 0.0340, 3500.0)]).unwrap(),
         }),
         Fuel::make(FuelDef {
-            name: String::from("NFFL02"),
-            desc: String::from("Timber (grass & understory)"),
+            name: String::try_from("NFFL02").unwrap(),
+            desc: String::try_from("Timber (grass & understory)").unwrap(),
             depth: Length::new::<foot>(1.0),
             mext: Ratio::new::<ratio>(0.15),
             adjust: Ratio::new::<ratio>(1.0),
-            particles: vec![
+            particles: Vec::from_slice(&[
                 ParticleDef::standard(ParticleType::Dead, 0.0920, 3000.0),
                 ParticleDef::standard(ParticleType::Dead, 0.0460, 109.0),
                 ParticleDef::standard(ParticleType::Dead, 0.0230, 30.0),
                 ParticleDef::standard(ParticleType::Herb, 0.0230, 1500.0),
-            ],
+            ]).unwrap(),
         }),
         Fuel::make(FuelDef {
-            name: String::from("NFFL03"),
-            desc: String::from("Tall Grass (2.5 ft)"),
+            name: String::try_from("NFFL03").unwrap(),
+            desc: String::try_from("Tall Grass (2.5 ft)").unwrap(),
             depth: Length::new::<foot>(2.5),
             mext: Ratio::new::<ratio>(0.25),
             adjust: Ratio::new::<ratio>(1.0),
-            particles: vec![ParticleDef::standard(ParticleType::Dead, 0.1380, 1500.0)],
+            particles: Vec::from_slice(&[ParticleDef::standard(ParticleType::Dead, 0.1380, 1500.0)]).unwrap(),
         }),
         Fuel::make(FuelDef {
-            name: String::from("NFFL04"),
-            desc: String::from("Chaparral (6 ft)"),
+            name: String::try_from("NFFL04").unwrap(),
+            desc: String::try_from("Chaparral (6 ft)").unwrap(),
             depth: Length::new::<foot>(6.0),
             mext: Ratio::new::<ratio>(0.2),
             adjust: Ratio::new::<ratio>(1.0),
-            particles: vec![
+            particles: Vec::from_slice(&[
                 ParticleDef::standard(ParticleType::Dead, 0.2300, 2000.0),
                 ParticleDef::standard(ParticleType::Dead, 0.1840, 109.0),
                 ParticleDef::standard(ParticleType::Dead, 0.0920, 30.0),
                 ParticleDef::standard(ParticleType::Wood, 0.2300, 1500.0),
-            ],
+            ]).unwrap(),
         }),
         Fuel::make(FuelDef {
-            name: String::from("NFFL05"),
-            desc: String::from("Brush (2 ft)"),
+            name: String::try_from("NFFL05").unwrap(),
+            desc: String::try_from("Brush (2 ft)").unwrap(),
             depth: Length::new::<foot>(2.0),
             mext: Ratio::new::<ratio>(0.2),
             adjust: Ratio::new::<ratio>(1.0),
-            particles: vec![
+            particles: Vec::from_slice(&[
                 ParticleDef::standard(ParticleType::Dead, 0.0460, 2000.0),
                 ParticleDef::standard(ParticleType::Dead, 0.0230, 109.0),
                 ParticleDef::standard(ParticleType::Wood, 0.0920, 1500.0),
-            ],
+            ]).unwrap(),
         }),
         Fuel::make(FuelDef {
-            name: String::from("NFFL06"),
-            desc: String::from("Dormant Brush & Hardwood Slash"),
+            name: String::try_from("NFFL06").unwrap(),
+            desc: String::try_from("Dormant Brush & Hardwood Slash").unwrap(),
             depth: Length::new::<foot>(2.5),
             mext: Ratio::new::<ratio>(0.25),
             adjust: Ratio::new::<ratio>(1.0),
-            particles: vec![
+            particles: Vec::from_slice(&[
                 ParticleDef::standard(ParticleType::Dead, 0.0690, 1750.0),
                 ParticleDef::standard(ParticleType::Dead, 0.1150, 109.0),
                 ParticleDef::standard(ParticleType::Dead, 0.0920, 30.0),
-            ],
+            ]).unwrap(),
         }),
-    ]
+    ]).unwrap()
     };
 }
